0f528b129cebfbf84e9b4f05a7194e16d9bbdba9
D00Med <heiselong@gmx.com>
2016-08-23 14:12:19 +1000

    flying pigs

--- init.lua
+++ init.lua
@@ -68,12 +68,74 @@ mobs:register_spawn("dmobs:panda", {"default:dirt_with_grass","ethereal:bamboo_d
 
 mobs:register_egg("dmobs:panda", "Panda", "default_papyrus.png", 1)
 
+mobs:register_mob("dmobs:pig", {
+	type = "animal",
+	passive = false,
+	reach = 1,
+	damage = 2,
+	attack_type = "dogfight",
+	hp_min = 12,
+	hp_max = 22,
+	armor = 130,
+	collisionbox = {-0.4, 0, -0.4, 0.4, 1, 0.4},
+	visual = "mesh",
+	mesh = "flying_pig.b3d",
+	textures = {
+		{"dmobs_flying_pig.png"},
+	},
+   jump = true,
+   fly = true,
+   fall_speed = 0,
+   stepheight = 10,
+	blood_texture = "mobs_blood.png",
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	runaway = true,
+	walk_velocity = 3,
+	run_velocity = 6,
+	run_chance = 20,
+	jump = true,
+	drops = {
+		{name = "mobs:meat_raw", chance = 1, min = 1, max = 1},
+	},
+	sounds = {
+      random = "mobs_pig",
+	  distance = 23,
+	},
+	water_damage = 0,
+	lava_damage = 2,
+	light_damage = 0,
+	follow = {"default:apple"},
+	view_range = 14,
+	animation = {
+		speed_normal = 10,
+		speed_run = 15,
+		walk_start = 1,
+		walk_end = 20,
+		stand_start = 1,
+		stand_end = 20,
+		run_start = 22,
+		run_end = 28,
+
+	},
+	on_rightclick = function(self, clicker)
+
+		if mobs:feed_tame(self, clicker, 8, true, true) then
+			return
+		end
+
+		mobs:capture_mob(self, clicker, 0, 5, 50, false, nil)
+	end,
+})
+
+mobs:register_spawn("dmobs:pig", {"default:pine_needles","default:leaves"}, 20, 10, 15000, 2, 31000)
+
+mobs:register_egg("dmobs:pig", "flying Pig", "wool_pink.png", 1)
+
 mobs:register_mob("dmobs:gnorm", {
 	type = "npc",
-	attacks_monsters = false,
 	can_dig = true,
-	passive = false,
-	group_attack = true,
+	passive = true,
 	reach = 1,
 	damage = 1,
 	attack_type = "dogfight",
@@ -504,7 +566,7 @@ mobs:register_egg("dmobs:orc", "Orc", "default_desert_sand.png", 1)
 mobs:register_mob("dmobs:ogre", {
 	type = "monster",
 	passive = false,
-	reach = 1,
+	reach = 2,
 	damage = 4,
 	attack_type = "dogfight",
 	hp_min = 22,
