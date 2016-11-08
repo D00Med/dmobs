baee602b7f5b0286a1b946f02c1a3625c9262212
D00Med <heiselong@gmx.com>
2016-11-08 14:22:53 +1000

    tortoise, rat, and butterflies.
    >added tortoise
    >added giant rat
    >added butterflies

--- init.lua
+++ init.lua
@@ -16,6 +16,38 @@ dmobs.destructive = false
 
 dofile(minetest.get_modpath("dmobs").."/baddies.lua")
 
+--butterflies
+
+minetest.register_abm({
+	nodenames = {"flowers:flower_rose", "flowers:flower_tulip", "flowers:flower_dandelion_yellow", "flowers:flower_viola", "flowers:flower_dandelion_white", "flowers:flower_geranium"},
+	interval = 10.0,
+	chance = 10,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		minetest.env:add_entity({x=pos.x,y=pos.y,z=pos.z}, "dmobs:butterfly")
+	end
+})
+
+minetest.register_entity("dmobs:butterfly", {
+	visual = "sprite",
+	physical = true,
+	textures = {"dmobs_butterfly.png",},
+	visual_size = {x=0.2, y=0.2},
+	on_activate = function(self)
+		num = math.random(1,4)
+		self.object:set_properties({textures = {"dmobs_butterfly"..num..".png",},})
+		minetest.after(5, function()
+		self.object:remove()
+		end)
+	end,
+	on_step = function(self)
+		local pos = self.object:getpos()
+		local vec = self.object:getvelocity()
+		self.object:setvelocity({x=-math.sin(12*pos.y), y=math.cos(12*pos.x), z=-math.sin(12*pos.y)})
+		self.object:setacceleration({x=-math.sin(6*vec.y), y=math.cos(6*vec.x), z=-math.sin(6*vec.y)})
+	end,
+	collisionbox = {0,0,0,0,0.1,0},
+})
+
 --wasps nest
 
 minetest.register_node("dmobs:hive", {
@@ -156,6 +188,64 @@ mobs:register_mob("dmobs:panda", {
 mobs:register_spawn("dmobs:panda", {"default:dirt_with_grass","ethereal:bamboo_dirt"}, 20, 10, 15000, 2, 31000)
 
 mobs:register_egg("dmobs:panda", "Panda", "default_papyrus.png", 1)
+
+mobs:register_mob("dmobs:tortoise", {
+	type = "animal",
+	passive = false,
+	reach = 1,
+	damage = 2,
+	attack_type = "dogfight",
+	hp_min = 6,
+	hp_max = 12,
+	armor = 130,
+	collisionbox = {-0.2, 0, -0.2, 0.2, 0.3, 0.2},
+	visual = "mesh",
+	mesh = "tortoise.b3d",
+	textures = {
+		{"dmobs_tortoise.png"},
+	},
+	blood_texture = "mobs_blood.png",
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	walk_velocity = 0.5,
+	run_velocity = 1,
+	jump = true,
+	float = true,
+	drops = {
+		{name = "default:stick", chance = 1, min = 1, max = 1},
+	},
+	water_damage = 0,
+	lava_damage = 2,
+	light_damage = 0,
+	fall_damage = 1,
+	fall_speed = -10,
+	fear_height = 4,
+	follow = {"default:papyrus"},
+	view_range = 14,
+	animation = {
+		speed_normal = 6,
+		speed_run = 10,
+		walk_start = 23,
+		walk_end = 43,
+		stand_start = 1,
+		stand_end = 20,
+		run_start = 23,
+		run_end = 43,
+
+	},
+	on_rightclick = function(self, clicker)
+
+		if mobs:feed_tame(self, clicker, 8, true, true) then
+			return
+		end
+
+		mobs:capture_mob(self, clicker, 0, 5, 50, false, nil)
+	end,
+})
+
+mobs:register_spawn("dmobs:tortoise", {"default:clay","default:sand"}, 20, 10, 15000, 2, 31000)
+
+mobs:register_egg("dmobs:tortoise", "Panda", "default_grass.png", 1)
 
 mobs:register_mob("dmobs:pig", {
 	type = "animal",
